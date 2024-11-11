cf66b813b76bf4908568255305df45bb1f0aeda8
arshad-codencoit <arshad-codencoit@gmail.com>
2024-11-12 02:48:18 +0600

    FAQ part done

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import React from "react";
+
+function BlogCard({ data }: { data: any }) {
+  return (
+    <div className="border border-[#DEDEDE] rounded-[40px] p-5">
+      <Image
+        src={data?.image}
+        alt="blog-cards"
+        style={{
+          width: "100%",
+          height: "auto",
+          marginBottom: 20,
+        }}
+      />
+      <h3 className="text-[18px] font-medium">{data?.title}</h3>
+      <div className="flex items-center justify-between">
+        <div className="w-[80%]">
+          <p className="text-[12px] my-3">{data?.description}</p>
+          <div className="text-[14px] flex items-center gap-5 mt-2">
+            <p className="font-semibold">{data.tag}</p>
+          </div>
+        </div>
+        <svg
+          width="50"
+          height="50"
+          viewBox="0 0 59 59"
+          className="cursor-pointer"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle
+            cx="29.5"
+            cy="29.5"
+            r="29.5"
+            transform="rotate(-90 29.5 29.5)"
+            fill="black"
+          />
+          <path
+            d="M37.7046 36.0916C37.7331 36.0917 38.2779 35.7487 38.9151 35.3294C39.5523 34.91 40.0611 34.5419 40.0459 34.5114C40.0305 34.4809 39.7245 34.0466 39.3657 33.5463C38.6453 32.5417 37.9517 31.2137 37.6443 30.2503C36.6865 27.2477 37.1273 24.1299 38.8657 21.6132L39.3083 20.9723L38.4656 20.0868L37.6229 19.2012L37.0659 19.6183C33.4166 22.3513 29.0426 22.0368 25.1648 18.7628C24.9116 18.549 24.6951 18.384 24.6836 18.396C24.5782 18.5068 23.2527 20.7842 23.2531 20.854C23.2533 20.9044 23.4851 21.1107 23.7682 21.3122C26.9027 23.544 30.4891 24.2717 33.2613 23.2385L33.9263 22.9907L24.681 32.7414L18.8349 38.9072L20.5403 40.6993L26.4228 34.5402L35.6799 24.8478L35.3994 25.7481C34.6716 28.0837 34.9697 30.8516 36.2497 33.6422C36.6144 34.4372 37.5967 36.0911 37.7046 36.0916Z"
+            fill="white"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+export default BlogCard;
---
+++ src/components/FAQ/FAQ.tsx
@@ -0,0 +1,118 @@
+"use client";
+import React, { useState } from "react";
+import { BiMinus, BiPlus } from "react-icons/bi";
+
+const faqData = [
+  {
+    id: 1,
+    question: "What are your focus areas as a UI/UX agency?",
+    answer:
+      "As a UI/UX agency, our core focus lies in understanding user needs through research, crafting intuitive designs with a strong emphasis on user interface and experience. We specialize in wireframing, prototyping, and collaboration with development teams to ensure seamless implementation, continually iterating based on user feedback for optimal digital experiences.",
+  },
+  {
+    id: 2,
+    question: "Why is UI/UX design important for your business growth?",
+    answer:
+      "As a UI/UX agency, our core focus lies in understanding user needs through research, crafting intuitive designs with a strong emphasis on user interface and experience. We specialize in wireframing, prototyping, and collaboration with development teams to ensure seamless implementation, continually iterating based on user feedback for optimal digital experiences.",
+  },
+  {
+    id: 3,
+    question: "Can your UX design agency help us with web design?",
+    answer:
+      "As a UI/UX agency, our core focus lies in understanding user needs through research, crafting intuitive designs with a strong emphasis on user interface and experience. We specialize in wireframing, prototyping, and collaboration with development teams to ensure seamless implementation, continually iterating based on user feedback for optimal digital experiences.",
+  },
+  {
+    id: 4,
+    question: "What process should I expect from your UX agency?",
+    answer:
+      "As a UI/UX agency, our core focus lies in understanding user needs through research, crafting intuitive designs with a strong emphasis on user interface and experience. We specialize in wireframing, prototyping, and collaboration with development teams to ensure seamless implementation, continually iterating based on user feedback for optimal digital experiences.",
+  },
+];
+
+function FAQ() {
+  const [activeFAQ, setActiveFAQ] = useState();
+
+  const toggleFAQ = (id: any) => {
+    if (id === activeFAQ) {
+      setActiveFAQ(undefined);
+    } else {
+      setActiveFAQ(id);
+    }
+  };
+
+  return (
+    <section className="mb-20 py-10">
+      <div className="container-fluid">
+        <div className="flex items-center gap-2">
+          <svg
+            width="50"
+            height="50"
+            viewBox="0 0 66 66"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_24_2303)">
+              <mask
+                id="mask0_24_2303"
+                maskUnits="userSpaceOnUse"
+                x="0"
+                y="0"
+                width="66"
+                height="66"
+              >
+                <path d="M66 0H0V66H66V0Z" fill="white" />
+              </mask>
+              <g mask="url(#mask0_24_2303)">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M1.44248e-06 9.12585e-07L2.48827e-06 16.5L33 33L7.21238e-07 49.5L0 66L33 49.5V66L66 49.5V33V16.5L33 0V16.5L1.44248e-06 9.12585e-07ZM33 16.5V33V49.5L66 33L33 16.5Z"
+                  fill="#E9B8FF"
+                />
+              </g>
+            </g>
+            <defs>
+              <clipPath id="clip0_24_2303">
+                <rect width="66" height="66" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+          <p className="text-5xl font-light">Frequently Asked Questions</p>
+        </div>
+        <div className="mt-8 divide-y-2">
+          {faqData?.map((item: any) => (
+            <div
+              key={item.id}
+              onClick={() => toggleFAQ(item.id)}
+              className="py-10 cursor-pointer"
+            >
+              <div className="flex items-center justify-between">
+                <h4 className="text-3xl">{item?.question}</h4>
+                {activeFAQ === item.id ? (
+                  <BiMinus size={35} />
+                ) : (
+                  <BiPlus size={35} />
+                )}
+              </div>
+              <div
+                className={`transform transition-all duration-500 ease-in-out overflow-hidden`}
+                style={{
+                  maxHeight: activeFAQ === item.id ? "500px" : "0",
+                  opacity: activeFAQ === item.id ? "1" : "0",
+                  transform:
+                    activeFAQ === item.id
+                      ? "translateY(0)"
+                      : "translateY(-10px)",
+                }}
+              >
+                <p className={`${activeFAQ === item?.id ? 'py-2' : 'py-0'} text-lg`}>{item.answer}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default FAQ;
